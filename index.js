f4dc8fe0423329d269a60cbaa83ca37d12f59bb3
james nyakundi <omarejames082@gmail.com>
2024-03-23 10:09:27 +0300

    Adds index.js

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 
-// Write your code here
+// Write your code herephase-1-arithmetic-lab
+// Variables
+let num1 = 31;
+let num2 = 2;
+let num3 = 22;
+let num4 = 6;
+
+// Multiplication
+let multiply = num1 * num2; // 31 * 2 = 62
+
+// Random integer
+let random = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER) + 1; // Generates a random integer greater than 0
+
+// Modulo
+let mod = num3 % num4; 
+
+// Maximum
+let numbers = [10, 5, 20, 15]; // Example set of numbers
+let max = Math.max(...numbers); // Returns the highest number in the set, which is 20
